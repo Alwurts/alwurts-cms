1a266193c17e3cce93071b3e17d6dd8ecff0190a
Alwurts <jota780@gmail.com>
2024-07-28 13:30:12 +0200

    Dynamic sitemap generation

--- alwurts-cms/src/app/(webpage)/sitemap.ts
+++ alwurts-cms/src/app/(webpage)/sitemap.ts
@@ -1,3 +1,6 @@
+export const dynamic = "force-dynamic";
+export const revalidate = 60 * 60 * 24;
+
 import { getPublishedPosts } from "@/server-actions/post";
 import type { MetadataRoute } from "next";
 
