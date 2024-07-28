faf67ca94827fa8ac3b9467d33017f464a7f3f11
Alwurts <jota780@gmail.com>
2024-07-28 13:22:06 +0200

    Use system theme at start for darkmode toggle

--- alwurts-cms/src/app/(webpage)/components/DarkModeToggle.tsx
+++ alwurts-cms/src/app/(webpage)/components/DarkModeToggle.tsx
@@ -1,30 +1,46 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useTheme } from "next-themes";
 import { Button } from "../components/ui/button";
 import { MoonIcon, SunIcon } from "lucide-react";
 
 const DarkModeToggle = () => {
 	const { systemTheme, theme, setTheme } = useTheme();
-	//const currentTheme = theme === "system" ? systemTheme : theme;
+	const [currentTheme, setCurrentTheme] = useState<string | undefined>(undefined);
+
+	useEffect(() => {
+		setCurrentTheme(theme === "system" ? systemTheme : theme);
+	}, [systemTheme, theme]);
+
+	const toggleTheme = () => {
+		if (currentTheme === "dark") {
+			setTheme("light");
+		} else if (currentTheme === "light") {
+			setTheme("system");
+		} else {
+			setTheme("dark");
+		}
+	};
+
+	if (currentTheme === undefined) return null;
 
 	return (
 		<Button
-			onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+			onClick={toggleTheme}
 			variant="ghost"
 			className="w-10 h-10 relative overflow-hidden"
 		>
 			<div
 				className="absolute inset-0 flex items-center justify-center transition-transform duration-300 ease-in-out"
 				style={{
-					transform: `translateY(${theme === "dark" ? "0%" : "-100%"})`,
+					transform: `translateY(${currentTheme === "dark" ? "0%" : "-100%"})`,
 				}}
 			>
 				<MoonIcon className="w-4 h-4" />
 			</div>
 			<div
 				className="absolute inset-0 flex items-center justify-center transition-transform duration-500 ease-in-out"
-				style={{ transform: `translateY(${theme === "dark" ? "100%" : "0%"})` }}
+				style={{ transform: `translateY(${currentTheme === "dark" ? "100%" : "0%"})` }}
 			>
 				<SunIcon className="w-4 h-4" />
 			</div>
